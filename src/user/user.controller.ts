55827a1aa4967ef81cb8e3e98dddbbeac6797be4
Maiky1304 <maiky-perlee@outlook.com>
2022-12-09 07:00:41 +0100

    add type to function params in controller + move @UseGuards() decorator class level

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { User } from '../auth/decorator/get-user.decorator';
+import { User as UserModel } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('/users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('/identify')
-  getMe(@User() user: any) {
+  getMe(@User() user: UserModel) {
     return user;
   }
 }
