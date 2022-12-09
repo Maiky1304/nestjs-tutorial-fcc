4f5b3d2b6c963b69cd1e667824592a8ac10f6b1a
Maiky1304 <maiky-perlee@outlook.com>
2022-12-09 06:57:37 +0100

    implement custom @User() decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
+import { User } from '../auth/decorator/get-user.decorator';
 
 @Controller('/users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('/identify')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@User() user: any) {
+    return user;
   }
 }
